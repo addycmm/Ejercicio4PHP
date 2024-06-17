b2780dc7eebed185d7e2c9f8565dea5c851e1ee3
addycmm <164122301+addycmm@users.noreply.github.com>
2024-06-17 07:54:14 -0400

    index.php

--- index.php
+++ index.php
@@ -1,16 +1,29 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "bdadriana";
-    $conexion = mysqli_connect($servername,$username,$password, $dbname);
-    $consulta = "SELECT * FROM persona";
-    $resultado = mysqli_query($conexion, $consulta);
-    $datos = array();
-
-    while ($fila = mysqli_fetch_assoc($resultado)) {
-        $datos[] = $fila;
-    }
-    mysqli_close($conexion);
-    echo json_encode($datos, JSON_UNESCAPED_UNICODE);
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style=" display:flex; align-content:center">
+    <h1>C#</h1>
+        <form action="https://localhost:44346/WebForm1.aspx" method="POST">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre"><br><br>
+            <label for="apellido">apellido:</label>
+            <input type="text" id="apellido" name="apellido"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+        <h1>JAVA</h1>
+        <form action="http://localhost:8083/E42A/NewServlet" method="POST">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre"><br><br>
+            <label for="apellido">apellido:</label>
+            <input type="text" id="apellido" name="apellido"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+        
+</body>
+</html>
